63fb0d1974c070435e53697b7412fa666c99e9b5
Ben Parish <parishben13@gmail.com>
2020-05-28 01:35:28 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -7,6 +7,8 @@ class CashRegister
     @items = []
   end
    
+   def add_item(item,price,quantity=1)
+   @items<< item[price]*quantity
   def apply_discount
     new_total = (@total*0.80)
     new_total
