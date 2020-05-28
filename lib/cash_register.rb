a4548160f718393cb8dec0a73cc6c92e96c983a4
Ben Parish <parishben13@gmail.com>
2020-05-28 01:31:58 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,12 +1,11 @@
 class CashRegister
   attr_accessor :items, :discount, :total, :last_transaction
   
-  def initialize(employee=nil)
-  @total = 0
- if !employee.nil?
-   apply_discount
+  def initialize(discount=0)
+    @total = 0
+    @discount = discount
+    @items = []
   end
- end
    
   def apply_discount
     new_total = (@total*0.80)
