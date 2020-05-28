631fa52c4b9b297eeda5378a3787d887a74f4080
Ben Parish <parishben13@gmail.com>
2020-05-28 01:38:08 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -8,12 +8,21 @@ class CashRegister
   end
    
    def add_item(item,price,quantity=1)
-   @items<< item[price]*quantity
+   @items<< item
+  @total += price*quantity
+  last_transaction = price*quantity
+end
   def apply_discount
     new_total = (@total*0.80)
     new_total
   end
-  
+  def add_item(title, amount, quantity=1)
+    self.total += amount * quantity
+    quantity.times do
+      items << title
+    end
+    self.last_transaction = amount * quantity
+  end
   def total
     @total
  end
