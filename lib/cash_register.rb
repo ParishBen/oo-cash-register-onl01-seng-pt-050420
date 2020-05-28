7db5eba047e88d5ba25d81b2f35e327012083c30
Ben Parish <parishben13@gmail.com>
2020-05-28 00:59:47 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,7 +1,10 @@
 class CashRegister
+  attr_accessor :total
   def initialize(total, employee_discount = 20)
   @total = total
   @total = 0 
+  end
+  def employee_discount
   end
   def total
 end
